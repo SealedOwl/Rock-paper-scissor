e4a052496858363e45d0c9b9c2036a1ddafa3e69
SealedOwl <157732808+SealedOwl@users.noreply.github.com>
2024-04-12 14:01:09 +0530

    play a round of RPS

--- scripts.js
+++ scripts.js
@@ -13,3 +13,30 @@ function getComputerChoice(){
     return compChoice;
 };
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    let result = ``;
+
+    if(playerSelection === computerSelection){
+        result += `
+        Your Choice: ${playerSelection}
+        Computer Choice: ${computerSelection}
+        It's a Tie!`;
+    }else if(
+        (playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper')){
+
+            result += `
+        Your Choice: ${playerSelection}
+        Computer Choice: ${computerSelection}
+        You Win!`;
+        }else{
+            result += `
+        Your Choice: ${playerSelection}
+        Computer Choice: ${computerSelection}
+        You Lose!`;
+        }
+
+    return result;
+};
